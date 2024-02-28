fa9848836affb6764e3db6e17f79d4cde8f7091e
dylpall <dylan.pallatroni.mymail.champlain.edu>
2024-02-28 01:28:07 +0000

    updated secure ssh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,8 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
+sudo cp /home/dylan/Tech-Journal/linux/public-keys/id_rsa.pub /home/sys265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh
+sudo chmod 600 /home/sys265/.ssh/authorized_keys
+sudo chown -R sys265:sys265 /home/sys265/.ssh
+
